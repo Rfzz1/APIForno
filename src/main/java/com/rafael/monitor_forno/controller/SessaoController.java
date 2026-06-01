a1142750e8acec3d46da201eea005371c2872c6e
Rafael Bressan Fiorio <rafinhafiorioofc@gmail.com>
2026-06-01 16:15:24 -0300

    feature: API CRUD (Builded service, repository and controller)

--- src/main/java/com/rafael/monitor_forno/controller/SessaoController.java
+++ src/main/java/com/rafael/monitor_forno/controller/SessaoController.java
@@ -1,11 +1,55 @@
 package com.rafael.monitor_forno.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.rafael.monitor_forno.database.model.Sessao;
+import com.rafael.monitor_forno.service.SessaoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
-@RequestMapping("v1/sessao")
+@RequestMapping("v1/sessoes")
 public class SessaoController {
+
+    private final SessaoService sessaoService;
+
+    public SessaoController(SessaoService sessaoService) {
+        this.sessaoService = sessaoService;
+    }
+
+    @PostMapping("/iniciar")
+    public ResponseEntity<Sessao> criarSessao(){
+
+        Sessao sessao = sessaoService.iniciarSessao();
+        return new ResponseEntity<>(sessao, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}/encerrar")
+    public ResponseEntity<Sessao> encerrarSessao(@PathVariable UUID id){
+
+        Sessao sessao = sessaoService.encerrarSessao(id);
+
+        return ResponseEntity.ok(sessao);
+
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Sessao>> pegarTodasSessoes() {
+
+        return ResponseEntity.ok(
+                sessaoService.findAll()
+        );
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Sessao> pegarSessaoPorId(@PathVariable UUID id) {
+
+        Sessao sessao = sessaoService.findById(id);
+
+        return ResponseEntity.ok(sessao);
+
+    }
+
 }

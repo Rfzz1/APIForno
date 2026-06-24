a6a38052814c2011f746682a9f443850e54250bb
Rafael Bressan Fiorio <rafinhafiorioofc@gmail.com>
2026-06-24 14:30:52 -0300

    fix: route errors

--- src/main/java/com/rafael/monitor_forno/controller/AuthController.java
+++ src/main/java/com/rafael/monitor_forno/controller/AuthController.java
@@ -13,6 +13,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/v1/auth")
+@CrossOrigin(origins="http://localhost:5173")
 public class    AuthController {
 
     private final UsuarioService usuarioService;
